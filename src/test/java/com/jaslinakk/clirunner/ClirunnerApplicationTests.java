8149cfd1e279d6ae61ca523ccd8750799881db8f
Jaslina Kalai Kuni <jaslina@jaslinas-mbp.lan>
2024-04-18 08:35:52 -0400

    init:

---
+++ src/test/java/com/jaslinakk/clirunner/ClirunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaslinakk.clirunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClirunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
